e94db9af77558e0a75c8a7cf272a33f2976a92a5
Fernando Moreno <fernando.morenorozo@wu.com>
2023-12-17 15:24:25 -0300

    Add modules

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,24 +1,29 @@
 variable "region" {
   description = "La región de AWS"
   type        = string
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block para la VPC"
   type        = string
+  default = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
   description = "Nombre de la VPC"
   type        = string
+  default = "mi-vpc"
 }
 
 variable "subnet_a_cidr" {
   description = "CIDR block para la Subred A"
   type        = string
+  default = "10.0.1.0/24"
 }
 
 variable "subnet_b_cidr" {
   description = "CIDR block para la Subred B"
   type        = string
+  default = "10.0.2.0/24"
 }
